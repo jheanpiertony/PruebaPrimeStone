4d5e0a16ba30ed159d9713d302f4e5e480ade616
Eder Ortega Cardona <eoc.zina@nokia.com>
2021-04-30 21:24:56 -0500

    feat: setup front

--- pruenaprimestone/urls.py
+++ pruenaprimestone/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from front.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('front/', include('front.urls', namespace='front')),
+    path('', HomeView.as_view(), name='home')
 ]
